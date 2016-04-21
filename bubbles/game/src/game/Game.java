107be7d962a569d7142cf62d4d1d0c90acdf07e1
thilothuma <thilothuma.08@gmail.com>
2016-04-21 16:19:45 +0530

    Update Game.java

--- bubbles/game/src/game/Game.java
+++ bubbles/game/src/game/Game.java
@@ -16,10 +16,10 @@ public class Game {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //new Guess();
+        new Guess();
        // new puzzle(0);
         //new Minesweeper();
-        new TTT1();
+        //new TTT1();
     }
     
 }
